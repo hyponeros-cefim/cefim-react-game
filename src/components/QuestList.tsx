d333c13ce102d03af124f6eae9fd5f0efbda6b71
Quentin Collet <q.hyponerso@gmail.com>
2025-05-13 00:38:52 +0200

    feat: ajouter la gestion de l'état des quêtes dans le composant QuestList

--- src/components/QuestList.tsx
+++ src/components/QuestList.tsx
@@ -1,4 +1,5 @@
 import { CircleCheckBigIcon, SquircleIcon } from 'lucide-react';
+import { useState } from 'react';
 
 interface Quest {
   id: number;
@@ -14,15 +15,23 @@ const QuestList = () => {
     { id: 3, state: false, name: 'Miner de la pierre', description: 'Description of Quest 3' },
     { id: 4, state: true, name: 'Couper du bois', description: 'Description of Quest 4' },
   ];
+  const [quests, setQuests] = useState<Quest[]>(questList);
+
+  const toggleQuestState = (id: number) => {
+    setQuests((prevQuests) => prevQuests.map((quest) => (quest.id === id ? { ...quest, state: !quest.state } : quest)));
+  };
 
   function renderQuest(quest: Quest) {
+    const { id, name, description, state } = quest;
     return (
-      <li key={quest.id}>
-        <button className="flex items-center cursor-pointer w-full p-3 text-base font-bold  rounded-lg group hover:shadow bg-gray-600 hover:bg-gray-500 text-white">
-          {quest.state ? <CircleCheckBigIcon color="#00ff00" /> : <SquircleIcon color="#c0c7c0" />}
+      <li key={id}>
+        <button
+          onClick={() => toggleQuestState(id)}
+          className="flex items-center cursor-pointer w-full p-3 text-base font-bold  rounded-lg group hover:shadow bg-gray-600 hover:bg-gray-500 text-white">
+          {state ? <CircleCheckBigIcon color="#00ff00" /> : <SquircleIcon color="#c0c7c0" />}
           <div className="flex flex-col items-start gap-2">
-            <span className="flex-1 ms-3 whitespace-nowrap">{quest.name}</span>
-            <span className="flex-1 ms-3  text-gray-300 font-medium ">{quest.description}</span>
+            <span className="flex-1 ms-3 whitespace-nowrap">{name}</span>
+            <span className="flex-1 ms-3  text-gray-300 font-medium ">{description}</span>
           </div>
         </button>
       </li>
@@ -38,7 +47,7 @@ const QuestList = () => {
     <div className="flex justify-end m-4">
       <div className="w-full max-w-sm p-4 border rounded-lg shadow-sm sm:p-6 bg-gray-800 border-gray-700">
         <h5 className="mb-3 text-base font-semibold  md:text-xl text-white">Liste des quêtes</h5>
-        <ul className="my-4 space-y-3">{questList.map((quest) => renderQuest(quest))}</ul>
+        <ul className="my-4 space-y-3">{quests.map((quest) => renderQuest(quest))}</ul>
       </div>
     </div>
   );
