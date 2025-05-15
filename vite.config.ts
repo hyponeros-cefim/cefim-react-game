e87377ade870033392bc53161a1aff8270f6948b
Quentin Collet <q.hyponerso@gmail.com>
2025-05-15 15:20:27 +0200

    feat: ajouter un workflow GitHub Actions pour déployer l'application Vite React sur GitHub Pages et remplacer l'icône Vite par une icône Skull

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/cefim-react-game/', // Mettre le nom du dépôt GitHub ici
   plugins: [react(), tailwindcss()],
-})
+});
+
+// Installer path et :
+// resolve: {
+//   alias: {
+//     '@': path.resolve(__dirname, 'src'),
+//   },
