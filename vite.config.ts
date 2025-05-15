2961df9bd25809f0b59ddace80c182c6491da22e
Quentin Collet <q.hyponerso@gmail.com>
2025-05-15 15:29:10 +0200

    feat: supprimer la base du chemin dans la configuration Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cefim-react-game/', // Mettre le nom du dépôt GitHub ici
   plugins: [react(), tailwindcss()],
 });
 
